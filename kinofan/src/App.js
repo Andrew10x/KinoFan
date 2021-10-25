badefd54bf5880ef231b1673113aa744bdf69bfb
Andrew <svt.andrey1631@gmail.com>
2021-10-25 19:08:20 +0300

    create header and movie page

---
+++ kinofan/src/App.js
@@ -0,0 +1,16 @@
+import Header from './pages/Header';
+import Movie from './pages/Movie';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+    <Header/>
+    <Movie/>  
+    </div>
+  );
+}
+
+export default App;
---
+++ kinofan/src/pages/Header/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react';
+import './header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="logo">KinoFun</div>
+            <div className="contacts">
+                <div className="phone">0974576333</div>
+                <div className="social-networks">
+                    <a href="#"><i class="fab fa-instagram-square" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fab fa-youtube-square" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fab fa-telegram" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </header>
+    )
+}
